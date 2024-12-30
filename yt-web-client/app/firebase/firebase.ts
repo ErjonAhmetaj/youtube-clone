521fb0abccae6f3424af8441aebdf474ec059536
Oni Ahmetaj <oni@Onis-Air.lan>
2024-12-29 21:56:52 -0500

    Created a frontend that allows the user to sign in and signout using their google login and using firebase SDK

---
+++ yt-web-client/app/firebase/firebase.ts
@@ -0,0 +1,47 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+    getAuth,
+    signInWithPopup,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    User
+ } from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBN5vzzbqvIBOwU44dOBBuLkypezu7Rp7Y",
+  authDomain: "yt-clone-a3589.firebaseapp.com",
+  projectId: "yt-clone-a3589",
+  appId: "1:174076657354:web:73a62f75fc3ed375edfe33",
+  measurementId: "G-273MXGSH9R"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+/**
+ * Signs the user in with a Google popup.
+ * @returns A promise that resolves with the user's credentials.
+ */
+export function signInWithGoogle() {
+  return signInWithPopup(auth, new GoogleAuthProvider());
+}
+
+/**
+ * Signs the user out.
+ * @returns A promise that resolves when the user is signed out.
+ */
+export function signOut() {
+  return auth.signOut();
+}
+
+/**
+ * Trigger a callback when user auth state changes.
+ * @returns A function to unsubscribe callback.
+ */
+export function onAuthStateChangedHelper(callback: (user: User | null) => void) {
+  return onAuthStateChanged(auth, callback);
+}
